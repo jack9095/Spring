160d2e4c8fa8dca255cfc5c43b37105f8588438d
wangfei <gaodun123com>
2018-11-06 15:36:33 +0800

    Redis简单使用

--- SpringMVC/src/main/java/com/kuanquan/demo/service/PersonService.java
+++ SpringMVC/src/main/java/com/kuanquan/demo/service/PersonService.java
@@ -3,9 +3,14 @@ package com.kuanquan.demo.service;
 import com.kuanquan.demo.entity.Person;
 
 public interface PersonService {
+    // 根据id查找具体用户
     Person findPersonById(long id);
+    // 根据id查找用户名称
     String findPersonName(long id);
+    // 根据id删除用户
     void deletePerson(long id);
+    // 更新用户信息
     void updataPerson(Person person);
+    // 插入用户信息
     void insertPerson(Person person);
 }
